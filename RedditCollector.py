1f4231087b621a40d32759cd2f186ea0e6ade5e3
Ginger879 <anewton879@gmail.com>
2016-01-06 17:18:48 -0800

    RedditCollector3 is our new working reddit scraping program!
    Everything else sucks!

--- RedditCollector.py
+++ RedditCollector.py
@@ -1,11 +1,15 @@
 import praw
 import json
+threadid = raw_input("Enter ID of thread to collect comments from: ")
 
 #DO NOT SET THIS TO ANYTHING UNDER 10
 total = 10
 threadname = ""
 
 p = praw.Reddit(user_agent='DisasterFinder')
+p.config.store_json_result = True
 comments = praw.helpers.comment_stream(p, 'news', total)
+f = open("jsonoutput.txt", 'w')
 for x in comments:
-    print x
+    if threadid == x.link_id:
+        f.write(json.dumps(x.json_dict) + '\n')
--- RedditDisasterFinder.py
+++ RedditDisasterFinder.py
@@ -1,13 +1,30 @@
 """ Prints information about the top ten links in /r/news """
 import praw
+from datetime import datetime
 
-#gets the top scoring links in the worldnews subreddit from the reddit api using praw
+
+#gets the top scoring links in the news subreddit from the reddit api using praw
 p = praw.Reddit(user_agent='DisasterFinder')
 submissions = p.get_subreddit('news').get_hot(limit=10)
 
 #prints each post's position on the subreddit, score title, id, and creation value
-print "index, score, title, id, created"
+dt = datetime.now()
+print "current date, index, score, title, id, date created"
 count = 1
+f = open("redditterms.txt", 'r')
+f = f.readlines()
+f = [x.rstrip('\n') for x in f]
 for x in submissions:
-    print str(count) + ", " + str(x.score) +", " + x.title + ", " + x.id + ", " + str(x.created)
+    title = x.title.replace('"', '\\\"')
+    titleL = title.lower()
+    for y in f:
+        if y in titleL:
+            print "%s, %d, %d, \"%s\", %s, %s" %(
+                dt, 
+                count, 
+                x.score, 
+                title, 
+                x.id, 
+                str(datetime.fromtimestamp(x.created)))
+            title = "Nothing to see here"
     count += 1
